c34409e8f11886b55005d8f59a1421f27432e582
RamadeviRamkumar <ramdevcse25@gmail.com>
2023-12-26 11:44:12 +0530

    redux

---
+++ src/CustomerView.js
@@ -0,0 +1,11 @@
+export default function CustomerView() {
+    return <div>
+        <h3>Customer List</h3>
+        <ul>
+            {
+                customers.map((customer)=><li>{customer}</li>)
+            }
+        </ul>
+    </div>
+    
+}
---
+++ src/Slices/CustomerSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const CustomerSlice = createSlice({
+    name : 'customer',
+    initialState,
+    reducers : {
+        addCustomer(state,action){
+          state.push(action.payload)
+        }
+    }
+})
+
+export const {addCustomer} = CustomerSlice.actions;
+export default CustomerSlice.reducer
